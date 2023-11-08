c8edcbeba28fa5fc10d5c2981971a03a5210b7b4
EC2 Default User <ec2-user@ip-172-31-9-171.us-east-2.compute.internal>
2023-11-08 18:09:21 +0000

    first commit

---
+++ route.spec.js
@@ -0,0 +1,10 @@
+const request = require('supertest') //importing the supertest library
+const app = require('./index.js') //importing the 'index.js' file
+
+describe('Get route', () => {
+  it('page should return hello world travis', async () => {
+    const res = await request(app).get('/')
+    expect(res.statusCode).toEqual(200)
+    expect(res.body).toEqual('Hello world from Travis')
+  })
+})
